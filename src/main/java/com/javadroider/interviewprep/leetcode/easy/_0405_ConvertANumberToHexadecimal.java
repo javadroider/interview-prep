0110187e077fae6ecc89228cf493ce78a22196e9
javadroider <javadroider@gmail.com>
2020-09-21 22:20:16 +0530

    Update _0405_ConvertANumberToHexadecimal.java

--- src/main/java/com/javadroider/interviewprep/leetcode/easy/_0405_ConvertANumberToHexadecimal.java
+++ src/main/java/com/javadroider/interviewprep/leetcode/easy/_0405_ConvertANumberToHexadecimal.java
@@ -2,6 +2,16 @@ package com.javadroider.interviewprep.leetcode.easy;
 
 public class _0405_ConvertANumberToHexadecimal {
 
+    public static void main(String[] args) {
+        _0405_ConvertANumberToHexadecimal instance = new _0405_ConvertANumberToHexadecimal();
+        int number = 380;
+        String hex = instance.toHex(number);
+        System.out.println(hex);
+        int convertedNumber = instance.toNumber(hex);
+        System.out.println(convertedNumber);
+        System.out.println(instance.hex_to_decimal(hex));
+    }
+
     //https://leetcode.com/problems/convert-a-number-to-hexadecimal/discuss/89253/Simple-Java-solution-with-comment
     public String toHex(int num) {
         /*
@@ -16,7 +26,7 @@ public class _0405_ConvertANumberToHexadecimal {
 
             1*16^2 + 7*16^1 + 12 * 16^0
             256 + 112 + 12 = 380
-            
+
          */
         if (num == 0) {
             return "0";
@@ -29,5 +39,35 @@ public class _0405_ConvertANumberToHexadecimal {
         }
 
         return hexValue;
+    }
+
+    //https://www.w3resource.com/java-exercises/basic/java-basic-exercise-28.php
+    public int hex_to_decimal(String s) {
+        String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16 * val + d;
+        }
+        return val;
+    }
+
+    private int toNumber(String hex) {
+        int ret = 0;
+        int count = 0;
+        for (int i = hex.length() - 1; i >= 0; i--) {
+            int num = 0;
+            char ch = hex.charAt(i);
+            if (ch >= 'a' && ch <= 'f') {
+                num = ch - 'a' + 10;
+            } else if (ch >= '0' && ch <= '9') {
+                num = ch - '0';
+            }
+            ret += Math.pow(16, count) * num;
+            count++;
+        }
+        return ret;
     }
 }
